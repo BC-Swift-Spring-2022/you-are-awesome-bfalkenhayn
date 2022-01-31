4e5b4e1624055a14936fc889b525627c70b4008c
Bridget Falkenhayn <bfalkenhayn01@gmail.com>
2022-01-31 12:28:31 -0500

    2.10 pictures and messages

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var awesomeLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     var imageNumber = 0
-    
+    var messageNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
        }
@@ -26,34 +26,46 @@ class ViewController: UIViewController {
     @IBAction func messageButtonPressed(_ sender: Any) {
       
              print(imageNumber)
+      
+        var messages = ["You are awesome", "You are Great", "You are fantastic", "Fabulous? That's you"]
        
-        imageNumber = imageNumber + 1
-       //  let imageName = "image" + String(imageNumber)
-        let imageName = "image\(imageNumber)"
-        imageView.image = UIImage(named: imageName)}
- 
-    if imageNumber == 10 {
-        imageNumber = 0
-    }
-         
-    //       let awesome = "You are awesome"
-                //
-                //        if awesomeLabel.text == awesome {
-                //            awesomeLabel.text = "You are Great"
-                //            imageView.image = UIImage(named: "image1")}
-                //        else if  awesomeLabel.text == "You are Great"
-                //        {awesomeLabel.text = "You are Da Bomb"
-                //            imageView.image = UIImage(named: "image2")}
-                //        else
-                //            {
-                //            awesomeLabel.text = awesome
-                //            imageView.image = UIImage(named: "image0")
-                //        }
-                //
+        print(messageNumber)
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+        awesomeLabel.text = messages[messageNumber]
         
+//
+
+        imageNumber = imageNumber + 1
+          var imageName = "image" + String(imageNumber)
+         if imageNumber == 9 {
+             imageNumber = 0}
+                imageView.image = UIImage(named: imageName)}
+        
+            
+
     }
+
+//           let awesome = "You are awesome"
+//
+//            if awesomeLabel.text == awesome {
+//                awesomeLabel.text = "You are Great"
+//                imageView.image = UIImage(named: "image1")}
+//            else if  awesomeLabel.text == "You are Great"
+//            {awesomeLabel.text = "You are Da Bomb"
+//                imageView.image = UIImage(named: "image2")}
+//            else
+//                {
+//                awesomeLabel.text = awesome
+//                imageView.image = UIImage(named: "image0")
+//            }
     
-     
+        
+
+    
+    
 
 
 
